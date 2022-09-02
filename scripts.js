e3999563391f083cc9911c474e73b1889e2def94
RingK27 <111756643+RingK27@users.noreply.github.com>
2022-09-02 23:01:39 +0200

    Basic functions
    Added basic functions

---
+++ scripts.js
@@ -0,0 +1,25 @@
+let operand = null;
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let a = 2;
+let b = 4;
+
+console.log(add(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
